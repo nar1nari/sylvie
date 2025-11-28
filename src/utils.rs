874456f0ef3f60ec934c7b178959ccec446d4aec
nar1nari <nar1nariq@proton.me>
2025-11-28 15:06:23 +0500

    Make reply_without_ping work with Strings and CreateReply

--- src/utils.rs
+++ src/utils.rs
@@ -9,14 +9,36 @@ pub const DEFAULT_EMBED_COLOR: Color = Color::from_rgb(0x2e, 0xcc, 0x70);
 pub const HELP_EMBED_COLOR: Color = Color::from_rgb(0x3b, 0x88, 0xc3);
 pub const ERROR_EMBED_COLOR: Color = Color::RED;
 
+pub trait IntoReply {
+    fn into_reply(self) -> poise::CreateReply;
+}
+
+impl IntoReply for String {
+    fn into_reply(self) -> poise::CreateReply {
+        poise::CreateReply::default().content(self)
+    }
+}
+
+impl IntoReply for &str {
+    fn into_reply(self) -> poise::CreateReply {
+        poise::CreateReply::default().content(self)
+    }
+}
+
+impl IntoReply for poise::CreateReply {
+    fn into_reply(self) -> poise::CreateReply {
+        self
+    }
+}
+
 pub async fn reply_without_ping(
     ctx: Context<'_>,
-    content: impl Into<String>,
+    content: impl IntoReply,
 ) -> Result<poise::ReplyHandle<'_>, Error> {
     Ok(ctx
         .send(
-            poise::CreateReply::default()
-                .content(content.into())
+            content
+                .into_reply()
                 .reply(true)
                 .allowed_mentions(serenity::CreateAllowedMentions::new()),
         )
